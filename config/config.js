808287889abc450a41c98eb6a776336489b29ead
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-02 13:09:51 +0700

    update config

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    host: process.env.HOST,
+    host: process.env.DATABASE_URL,
     dialect: process.env.DIALECT,
     operatorsAliases: false
   }
